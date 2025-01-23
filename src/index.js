446ac92ea30c8ecc33c920b4da0168fd6ccc09b1
vkuzmanova <42387114+vkuzmanova@users.noreply.github.com>
2025-01-23 16:25:00 +0200

    install and setup express

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log('It works')
+import express from 'express';
+
+const app = express();
+
+app.get('/', (res, req) => {
+    res.send('It works');
+});
+
+app.listen(5000, () => console.log('Server is listening on http://localhost:5000...'));
