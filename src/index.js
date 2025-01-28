d8cc45fe9ac185ba5ad59b43a3edc6ed51298245
vkuzmanova <42387114+vkuzmanova@users.noreply.github.com>
2025-01-28 17:44:49 +0200

    Add about page

--- src/index.js
+++ src/index.js
@@ -16,4 +16,8 @@ app.get('/', (req, res) => {
     res.render('home');
 });
 
+app.get('/about', (req,res) => {
+    res.render('about');
+});
+
 app.listen(5000, () => console.log('Server is listening on http://localhost:5000...'));
