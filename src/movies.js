0fb527b60f466bd5ef98b03548b8498ad7653818
vkuzmanova <42387114+vkuzmanova@users.noreply.github.com>
2025-01-29 18:17:39 +0200

    Add dynamic rendering movies

---
+++ src/movies.js
@@ -0,0 +1,15 @@
+export default [
+    {
+        title: 'Jungle Cruise',
+        genre: 'Adventure',
+        description: 'Dreaming about saving countless lives and having another adventure, the feisty English feminist and doctor of botany, Dr Lily Houghton, embarks on a peril-laden mission to change the world. Along with her fashionable brother, MacGregor, Dr Houghton enlists the help of the arrogant, wisecracking riverboat skipper, Captain Frank Wolff, to guide them through the serpentine Amazon River in La Quila, his swift wooden boat. Now, as the intrepid trio ventures deeper and deeper into the heart of an impenetrable green maze, searching for something that cannot be found, a centuriesZ-old curse and the ruthless aristocrat, Prince Joachim, threaten to put an end to their ambitious plans.',
+        imageUrl: './static/img/jungle-cruise.jpeg',
+    },
+    {
+        title: 'Men of Steel',
+        genre: 'Super hero',
+        description: 'An alien child is evacuated from his dying world and sent to Earth to live among humans. His peace is threatened when other survivors of his home planet invade Earth.',
+        imageUrl: './static/img/the-little-mermaid.jpg',
+    },
+
+];
--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,4 @@
-import Router from 'express';
+import { Router } from 'express';
 import homeController from './controllers/home-controller.js'
 
 const routes = Router();
