79ee20cc0394c1096d4deaebfbcfa5a01d37bd67
vkuzmanova <42387114+vkuzmanova@users.noreply.github.com>
2025-01-30 18:48:21 +0200

    add search filter

--- src/controllers/movie-controller.js
+++ src/controllers/movie-controller.js
@@ -4,8 +4,9 @@ import movieService from '../services/movie-service.js';
 const movieController = Router();
 
 movieController.get('/search', (req,res) =>{
-    const movies = movieService.getAll();
-    
+    const filter = req.query;
+    const movies = movieService.getAll(filter);
+
     res.render('search', { movies });
 })
 
