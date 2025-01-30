ed3bb6fa825dfea99afae27d56ee3790e1fc8732
vkuzmanova <42387114+vkuzmanova@users.noreply.github.com>
2025-01-30 14:47:57 +0200

    parse form data on server

--- src/controllers/movie-controller.js
+++ src/controllers/movie-controller.js
@@ -1,21 +1,27 @@
 import { Router } from 'express';
 
-import movieService, { findMovie } from '../services/movie-service.js';
+import movieService from '../services/movie-service.js';
 
 const movieController = Router();
 
-import { movieController } from "../services/movie-service.js"
+//import { movieController } from "../services/movie-service.js"
 
 movieController.get('/create', (req,res) => {
     res.render('create');
 });
 
+movieController.post('/create', (req, res) =>{
+    const newMovie = req.body;
+    console.log(req.body);
+    res.end();
+})
+
 movieController.get('/:movieId/details', (req,res) => {
     const movieId = req.params.movieId;
-    console.log(movieId);
+  
     const movie = movieService.findOne(movieId);
-    console.log(movie);
-    res.render('details');
+   
+    res.render('details', { movie });
 })
 
 export default movieController;
