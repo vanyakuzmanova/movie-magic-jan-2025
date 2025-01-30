abea9ea5ff5c12d69ded9b65b89f60897865b8b1
vkuzmanova <42387114+vkuzmanova@users.noreply.github.com>
2025-01-30 16:28:25 +0200

    Extract movie partial

--- src/controllers/movie-controller.js
+++ src/controllers/movie-controller.js
@@ -3,7 +3,11 @@ import movieService from '../services/movie-service.js';
 
 const movieController = Router();
 
-//import { movieController } from "../services/movie-service.js"
+movieController.get('/search', (req,res) =>{
+    const movies = movieService.getAll();
+    
+    res.render('search', { movies });
+})
 
 movieController.get('/create', (req,res) => {
     res.render('create');
