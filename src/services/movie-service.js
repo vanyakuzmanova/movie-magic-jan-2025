ddae63cea487e771920c6845da421e290785760e
vkuzmanova <42387114+vkuzmanova@users.noreply.github.com>
2025-02-12 16:44:07 +0200

    refactor search page

--- src/services/movie-service.js
+++ src/services/movie-service.js
@@ -4,19 +4,21 @@ import Movie from '../models/Movie.js'
 
 export default{
     getAll( filter = {}){
-        //let result =  Movie.find();
+        let query =  Movie.find({});
 
-        // if(filter.search){
-        //     result = result.filter(movie => movie.title.toLowerCase().includes(filter.search.toLowerCase()));
-        // }
-        // if(filter.genre){
-        //     result = result.filter(movie => movie.genre.toLowerCase().includes(filter.genre.toLowerCase()));
-        // }
-        // if(filter.year){
-        //     result = result.filter(movie => movie.year == filter.year);
-        // }
+        //todo add case insensitive search
+         if(filter.search){
+             query = query.where({title: filter.search});
+         }
+         //todo add case insensitive search
+        if(filter.genre){
+            query = query.where({ genre: filter.genre});
+        }
+        if(filter.year){
+            query = query.where({year: Number(filter.year)});
+        }
 
-        return Movie.find();
+        return query;
     },
     getOne(movieId){
         //TODO: if movie is missing
