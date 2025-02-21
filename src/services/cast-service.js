7bfe4a7b1dce8ee9e255cb6d3a5709e9c173b739
vkuzmanova <42387114+vkuzmanova@users.noreply.github.com>
2025-02-21 11:36:02 +0200

    exclude cast that is already attach

--- src/services/cast-service.js
+++ src/services/cast-service.js
@@ -1,8 +1,16 @@
 import Cast from '../models/Cast.js';
 
 export default{
-    getAll(){
-        return Cast.find({});
+    getAll(filter = {}){
+        let query = Cast.find({});
+            if(filter.exclude){
+                //Mongoose
+                //query = query.find({_id: {$nin: filter.exclude }});
+                //Mongo
+                query = query.nin('_id', filter.exclude);
+            }
+
+        return query;
     },
     create(castData){
 
