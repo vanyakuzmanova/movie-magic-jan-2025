1fb8fbfe3672bc0b43e474b989bb91654bf52d27
vkuzmanova <42387114+vkuzmanova@users.noreply.github.com>
2025-02-21 17:02:59 +0200

    Update navigation

--- src/controllers/auht-controller.js
+++ src/controllers/auht-controller.js
@@ -2,4 +2,8 @@ import { Router } from "express";
 
 const authController = Router();
 
+authController.get('/', (req, res) =>{
+    res.render('auth/register', )
+});
+
 export default authController;
