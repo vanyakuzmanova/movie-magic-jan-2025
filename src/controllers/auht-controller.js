31ead1eaed49c6a7424acedd48842dd928c85415
vkuzmanova <42387114+vkuzmanova@users.noreply.github.com>
2025-02-22 19:01:29 +0200

    Add login functionality with jwt generation

--- src/controllers/auht-controller.js
+++ src/controllers/auht-controller.js
@@ -3,11 +3,11 @@ import authService from "../services/auth-service.js";
 
 const authController = Router();
 
-authController.get('/register', (req, res) =>{
-    res.render('auth/register' )
+authController.get('/register', (req, res) => {
+    res.render('auth/register')
 });
 
-authController.post('/register', async (req, res) =>{
+authController.post('/register', async (req, res) => {
     const userData = req.body;
 
     await authService.register(userData);
@@ -18,7 +18,21 @@ authController.post('/register', async (req, res) =>{
 authController.get('/login', (req, res) => {
     res.render('auth/login');
 
-   // res.end();
-})
+});
+
+authController.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        const token = await authService.login(email, password);
+        console.log(token)
+    } catch (err){
+        console.log(err.message);
+        return res.redirect('/404')
+    }
+
+
+    res.redirect('/');
+});
 
 export default authController;
--- src/services/auth-service.js
+++ src/services/auth-service.js
@@ -1,7 +1,35 @@
 import User from "../models/User.js";
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
+const SECRET = 'MYSECRET'
 export default{
     register(userData){
         return User.create(userData);
+    },
+    async login(email, password){
+        
+        //check if user exist
+        const user = await User.findOne( {email} );
+        if(!user){
+            throw new Error('Invalid email');
+        };
+        
+        //check password is correct
+        const isValid = await bcrypt.compare(password, user.password);
+        if(!isValid){
+            throw new Error('Invalid password');
+        }
+        
+        //generate token
+        const payload = {
+            id: user.id,
+            email: user.email
+        };
+        const token = jwt.sign(payload, SECRET, {expiresIn:'2h'});
+
+        //return 
+
+        return token;
     }
 };
