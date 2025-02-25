b5e9d334f8542cef6e43c05c7ac0576db6242490
vkuzmanova <42387114+vkuzmanova@users.noreply.github.com>
2025-02-25 16:17:21 +0200

    Show dynamic select value

--- src/controllers/movie-controller.js
+++ src/controllers/movie-controller.js
@@ -68,11 +68,33 @@ movieController.get('/:movieId/delete', async (req,res) => {
     res.redirect('/');
 });
 
+
 movieController.get('/:movieId/edit', async (req, res) =>{
     const movieId = req.params.movieId;
-    const movie = await movieService.getOne(movieId);
+    const movie = await movieService.getOne(movieId);   
 
-    res.render('movie/edit', { movie });
+    const categories = getCategoriesViewData(movie.category);
+    console.log(categories)
+
+    res.render('movie/edit', { movie, categories });
 })
+
+function getCategoriesViewData(category){
+    const categoriesMap = {
+        'tv-show': 'TV Show',
+        'animation': 'Animation',
+        'movie': 'Movie',
+        'documentary': 'Documentary',
+        'short-film': 'Short-film',
+    }
+    const categories = Object.keys(categoriesMap).map(value =>({
+        value,
+        label: categoriesMap[value],
+        selected: value === category ? 'selected' : '',
+    }))
+
+    return categories;
+}
+
 
 export default movieController;
